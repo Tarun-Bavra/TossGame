a899a7b96c36302cce83266f5a9b29591e74690a
Tarun <tarunbavra@gmail.com>
2024-06-29 10:53:47 +0530

    toss game

--- script.js
+++ script.js
@@ -1,18 +1,43 @@
 let btns = document.querySelectorAll("button");
-
+let output = document.querySelector(".output");
+let message = document.querySelector(".message");
 for (let t of btns) {
   t.addEventListener("click", () => {
     performToss(t);
   });
 }
+let userScore = 0;
+let computerScore = 0;
 function performToss(btn) {
   let buttonSelected = btn.innerHTML.toLowerCase();
   let tossValue = ["head", "tail"];
-  let choices = Math.floor(Math.random() * tossValue.length);
-  if (buttonSelected == tossValue[choices]) {
-console.log("wins")
+  let randomIndex = parseInt(Math.random() * tossValue.length);
+  if (buttonSelected == tossValue[randomIndex]) {
+    userScore++;
+    message.innerHTML = `<p>User wins</p>`
+    message.style.cssText = `
+    color:green;
+    background-color:yellow;
+    text-align:center;
+    width:300px;
+    border-radius:20px;
+    `
   }
   else{
-    console.log("loss")
+    computerScore++;
+    message.innerHTML = `<p>Computer wins</p>`
+    message.style.cssText = `
+    color:red;
+    background-color:black;
+    text-align:center;
+    width:300px;
+    border-radius:20px;
+    `
   }
+
+  output.innerHTML = `
+  userScore:${userScore}<br>
+  computerScore:${computerScore}
+  `
+  
 }
